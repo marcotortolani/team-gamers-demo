39ef1e436ea062d7c9d2a3d162a472f5c49c6b66
Marco Tortolani <mjtorto@gmail.com>
2024-03-18 10:39:59 -0300

    refactor component

---
+++ src/app/components/ui/TitleSection.jsx
@@ -0,0 +1,23 @@
+export function TitleSection({ icon, title, outline, borderColor }) {
+  const Icon = icon
+  return (
+    <h2
+      className={` ${
+        outline
+          ? 'px-4 py-[0.1rem]  bg-Black uppercase font-medium text-lg gap-3 border-[1px] rounded-full'
+          : 'text-xl gap-2'
+      } ${borderColor} flex items-center justify-center  text-White `}
+    >
+      <div className={`${outline ? 'w-6 h-6' : 'w-7 h-7'} `}>
+        <Icon width={'100%'} height={'100%'} />
+      </div>
+      <span
+        className={`${
+          outline ? ' border-none' : 'border-b-4 leading-6 border-b-Primary'
+        } `}
+      >
+        {title}
+      </span>
+    </h2>
+  )
+}
