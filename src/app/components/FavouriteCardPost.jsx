0bdbac78e9d1ac71552db779d8f361de02b839dd
Marco Tortolani <mjtorto@gmail.com>
2024-03-20 13:16:54 -0300

    fix hydratation and errors

--- src/app/components/FavouriteCardPost.jsx
+++ src/app/components/FavouriteCardPost.jsx
@@ -1,29 +1,38 @@
 'use client'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 import { useContext } from 'react'
 import { StateContext } from '@/providers/StateProvider'
 import ReactHtmlParser from 'react-html-parser'
 import ButtonLikeFav from './ui/ButtonLikeFav.jsx'
 
-import { poppinsReg400, poppinsReg600 } from '../../utils/fonts.js'
 import ImageMissing from './ImageMissing.jsx'
 import ButtonSeePost from './ui/ButtonSeePost.jsx'
 
 export default function FavouriteCardPost() {
+  const [favPosts, setFavPosts] = useState([])
   const { favouritePosts } = useContext(StateContext)
+
+  useEffect(() => {
+    if (favouritePosts !== 'undefined') {
+      setFavPosts(favouritePosts)
+    }
+  }, [favouritePosts])
 
   return (
     <div className=" w-screen px-4 md:w-full h-full flex justify-center ">
-      {favouritePosts?.length >= 1 ? (
+      {favPosts?.length >= 1 ? (
         <ul className=" w-full h-full  flex flex-col items-center gap-2 md:gap-6">
-          {favouritePosts?.map((post, index) => (
-            <FavouriteCard key={post.id} post={post} index={index} />
+          {favPosts?.map((post, index) => (
+            <li key={post.id}>
+              <FavouriteCard post={post} index={index} />
+            </li>
           ))}
         </ul>
       ) : (
-        'No hay favoritos agregados'
+        <div className="bg-Secondary w-5/6 max-w-[400px] h-32 flex items-center justify-center font-normal rounded-xl">
+          No hay favoritos agregados
+        </div>
       )}
     </div>
   )
@@ -31,7 +40,7 @@ export default function FavouriteCardPost() {
 
 export function FavouriteCard({ post, index }) {
   return (
-    <li
+    <div
       className={` relative w-full h-full max-h-[200px] md:min-h-[300px] md:max-h-[300px] lg:max-h-[380px] p-4 lg:p-8 my-2 mt-0 grid grid-cols-2 lg:grid-cols-5 gap-2 md:gap-4 lg:gap-6 rounded-xl md:rounded-xl lg:rounded-2xl ${
         index % 2 !== 0 ? 'bg-Primary ' : ' bg-Secondary  '
       } `}
@@ -93,6 +102,6 @@ export function FavouriteCard({ post, index }) {
           />
         </div>
       </div>
-    </li>
+    </div>
   )
 }
