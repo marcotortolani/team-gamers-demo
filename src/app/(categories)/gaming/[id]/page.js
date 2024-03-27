50452e7c245da7f8cf77cb0a3c15fe3b3ef989f3
Marco Tortolani <mjtorto@gmail.com>
2024-03-27 12:03:02 -0300

    fix import

--- src/app/(categories)/gaming/[id]/page.js
+++ src/app/(categories)/gaming/[id]/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CAT_EDITORIAL as cat } from '@/utils/static_data'
+import { CAT_GAMERS as cat } from '@/utils/static_data'
 import SectionRecommended from '@/app/components/SectionRecommended'
 import PagePost from '@/app/components/PagePost'
 
@@ -11,7 +11,7 @@ export default function page({ params }) {
       className={`z-0 text-White mt-36 w-full h-full min-h-screen px-4 flex flex-col items-center gap-4`}
     >
       <PagePost id={id} />
-      <SectionRecommended category={cat} qty={2} />
+      <SectionRecommended category={cat.gaming} qty={2} />
 
       <div className="w-full h-20"></div>
     </main>
