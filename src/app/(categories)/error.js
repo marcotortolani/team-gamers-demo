b5cd2505424859d766a13b89b835f88dcae4bbfc
Marco Tortolani <mjtorto@gmail.com>
2024-03-27 12:05:18 -0300

    fix colors name

--- src/app/(categories)/error.js
+++ src/app/(categories)/error.js
@@ -8,11 +8,11 @@ export default function error({ error }) {
   return (
     <main className=' z-0 mt-36 w-full   h-full min-h-screen px-4 flex flex-col items-center justify-between '>
       <div className=' w-full md:w-5/6 lg:w-4/6 lg:max-w-[900px] flex flex-col items-center gap-6'>
-        <div className='relative w-2/3 h-1/2 p-4 lg:min-h-[200px] flex flex-col items-center justify-center gap-2 bg-EpaPrimaryDark rounded-2xl'>
+        <div className='relative w-2/3 h-1/2 p-4 lg:min-h-[200px] flex flex-col items-center justify-center gap-2 bg-PrimaryDark rounded-2xl'>
           <span
             className={
               openSans.className +
-              ' text-4xl uppercase font-semibold text-EpaWhite'
+              ' text-4xl uppercase font-semibold text-White'
             }
           >
             Atencion!
@@ -20,7 +20,7 @@ export default function error({ error }) {
           <h2
             className={
               poppinsReg500.className +
-              ' px-10 text-center text-xl text-EpaWhite'
+              ' px-10 text-center text-xl text-White'
             }
           >
             La categoría o el post al que queres acceder no existe
@@ -32,7 +32,7 @@ export default function error({ error }) {
         <Link
           className={
             poppinsReg500.className +
-            ' px-4 py-2 font-semibold bg-EpaSecondary rounded-lg'
+            ' px-4 py-2 font-semibold bg-Secondary rounded-lg'
           }
           href={'/'}
           target='_self'
