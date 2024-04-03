fc145f395be529f3b58ad00570699d9cce7d6663
Marco Tortolani <mjtorto@gmail.com>
2024-04-03 15:11:50 -0300

    adding dynamic gamers images

--- src/app/components/CardGamer.jsx
+++ src/app/components/CardGamer.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
-export async function CardGamer({ path, gamerData, miniCard }) {
+export function CardGamer({ path, gamerData, miniCard }) {
+  const [imageError, setImageError] = useState(false)
+  const imageGamer = `https://raw.githubusercontent.com/marcotortolani/team-gamers-images/main/gamers/${gamerData.slug}.webp`
+
+  function handleImageError() {
+    setImageError(true)
+  }
+
   return (
     <Link
       className="z-30 relative w-full h-full flex justify-center items-center hover:cursor-pointer  "
@@ -12,15 +21,26 @@ export async function CardGamer({ path, gamerData, miniCard }) {
           miniCard
             ? 'w-full min-w-[80px] max-w-[120px] md:max-w-[180px] lg:max-w-[220px]  aspect-square md:aspect-video border-[0px] '
             : 'w-full max-w-[200px] aspect-[3/2] border-[0px]'
-        } relative flex items-center border-transparent shadow-inner hover:scale-x-110 transition-all duration-200 ease-in-out shadow-black h-full bg-gradient-to-br from-Secondary to-pink-800 rounded-xl md:rounded-2xl`}
+        } z-0 relative flex items-center border-transparent shadow-inner hover:scale-x-110 transition-all duration-200 ease-in-out shadow-black h-full bg-gradient-to-br from-Secondary to-pink-800 rounded-xl md:rounded-2xl`}
       >
         <div
-          className={` z-20 relative w-full flex items-center justify-center  object-cover `}
+          className={` z-20 relative w-full h-full flex items-center justify-center  object-cover rounded-[inherit] `}
         >
+          {!imageError && (
+            <Image
+              className="z-10 absolute top-0 w-full h-auto object-cover rounded-[inherit]"
+              fill
+              src={imageGamer}
+              alt={`${gamerData.name} Gamer Image`}
+              onError={handleImageError}
+            />
+          )}
           <h4
             className={`${
-              miniCard ? ' text-sm xs:text-lg lg:text-xl text-shadow' : ' text-lg md:text-2xl text-shadow'
-            }  w-5/6 line-clamp-2  flex items-center justify-center uppercase  shadow-Black font-extrabold text-center overflow-hidden cursor-default pointer-events-none text-White`}
+              miniCard
+                ? ' text-sm xs:text-lg lg:text-xl text-shadow'
+                : ' text-lg md:text-2xl text-shadow'
+            } z-30  w-5/6 line-clamp-2  flex items-center justify-center uppercase  shadow-Black font-extrabold text-center overflow-hidden cursor-default pointer-events-none text-White`}
           >
             {gamerData.name}
           </h4>
