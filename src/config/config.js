baf07b5c99b18d6fd9f248c7f1cd923606bf3632
Marco Tortolani <mjtorto@gmail.com>
2024-06-27 13:01:15 -0300

    feat api validation hash

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ const URL_CONTENT_BASE = 'http://content.ve.movistar.teamgamers.moob.club/'
 
 // ---------------------------------------------------------------------
 export const API_CONTENT = URL_CONTENT_BASE + 'wp-json/wp/v2/'
-export const ENDPOINT_VALIDATION_HASH =
-  URL_CONTENT_BASE + 'api/v1/validate_hash/'
 export const ENDPOINT_LANDING = URL_CONTENT_BASE + 'api/v1/landing/'
+
+export const ENDPOINT_VALIDATION_HASH =
+  'http://ws.movistar.ve.moob.club:13076/api/v1/subscriptions/isValid/'
