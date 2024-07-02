18295234f6240072cb8a406898b38c028997035b
Marco Tortolani <mjtorto@gmail.com>
2024-07-02 13:06:41 -0300

    refactor layout

--- src/app/(categories)/layout.js
+++ src/app/(categories)/layout.js
@@ -8,28 +8,35 @@ const cryptoUser = new NextCrypto('user enabled')
 const cryptoTrial = new NextCrypto('trial')
 
 export default async function layout({ children }) {
-  const activeUser = cookies().get(tokenActiveUser.name)
-  const activeTrial = cookies().get(tokenActiveTrial.name)
   let userEnabled = false
   let userTrial = 0
 
-  userTrial = await cryptoTrial.decrypt(activeTrial.value).then((res) => {
-    return res
-  })
+  try {
+    const activeUser = cookies().get(tokenActiveUser.name)
+    const activeTrial = cookies().get(tokenActiveTrial.name)
 
-  userEnabled = await cryptoUser.decrypt(activeUser.value).then((res) => {
-    return res
-  })
+    if (activeUser) {
+      userEnabled = await cryptoUser.decrypt(activeUser.value)
+    }
 
-  const ChildrenAuth = () => {
-    if (activeUser && userEnabled === 'true') return children
-    if (userTrial > 0) return <TrialProvider>{children}</TrialProvider>
-    return <SubscribeCard />
+    if (activeTrial) {
+      userTrial = parseInt(await cryptoTrial.decrypt(activeTrial.value), 10)
+    }
+  } catch (error) {
+    console.log('Error decrypting cookies:', error)
   }
 
   return (
-    <div className=" mt-16 md:mt-24 lg:mt-32 w-full h-full flex flex-col items-center relative ">
-      <ChildrenAuth />
+    <div className="mt-16 md:mt-24 lg:mt-32 w-full h-full flex flex-col items-center relative">
+      <ChildrenAuth userEnabled={userEnabled} userTrial={userTrial}>
+        {children}
+      </ChildrenAuth>
     </div>
   )
+}
+
+const ChildrenAuth = ({ children, userEnabled, userTrial }) => {
+  if (userEnabled === 'true') return children
+  if (userTrial > 0) return <TrialProvider>{children}</TrialProvider>
+  return <SubscribeCard />
 }
