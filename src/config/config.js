d126aae53f48ad3fd300a0bb7a994bcbed8046f5
Marco Tortolani <mjtorto@gmail.com>
2024-07-03 15:24:42 -0300

    update new validate endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,9 @@
-//const URL_CONTENT_BASE = 'http://content.mx.telcel.epamujer.moob.club/';
 const URL_CONTENT_BASE = 'http://content.ve.movistar.teamgamers.moob.club/'
-
 // ---------------------------------------------------------------------
 export const API_CONTENT = URL_CONTENT_BASE + 'wp-json/wp/v2/'
 export const ENDPOINT_LANDING = URL_CONTENT_BASE + 'api/v1/landing/'
-
 export const ENDPOINT_VALIDATION_HASH =
-  'http://ws.movistar.ve.moob.club:13076/api/v1/subscriptions/isValid/'
+  URL_CONTENT_BASE + 'wp-json/api/v1/validate_hash/'
 
 // export const ENDPOINT_VALIDATION_HASH =
-//   URL_CONTENT_BASE + 'api/v1/validate_hash/'
+//   'http://ws.movistar.ve.moob.club:13076/api/v1/subscriptions/isValid/'
