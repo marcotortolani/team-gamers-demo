314c5c1e284781f3a5a04b4344932b4bfbe05f07
Marco Tortolani <mjtorto@gmail.com>
2024-07-04 15:20:38 -0300

    refactoring layout

--- src/app/(categories)/layout.js
+++ src/app/(categories)/layout.js
@@ -1,33 +1,20 @@
 import { cookies } from 'next/headers'
-import SubscribeCard from '../components/SubscribeCard'
 import { TrialProvider } from '@/providers/TrialProvider'
 import NextCrypto from 'next-crypto'
 
 const cryptoUser = new NextCrypto('user enabled')
-const cryptoTrial = new NextCrypto('trial')
 
 export default async function Layout({ children }) {
   const cookieStore = cookies()
   const activeUser = cookieStore.get('enabledUser')
-  //const activeTrial = cookieStore.get('trial')
-
   let userEnabled = false
-  //let userTrial = 0
 
   if (activeUser) {
     userEnabled = (await cryptoUser.decrypt(activeUser.value)) === 'true'
   }
 
-  // if (activeTrial) {
-  //   userTrial = parseInt(await cryptoTrial.decrypt(activeTrial.value), 10)
-  // }
-
-  //console.log(userTrial)
-
   const ChildrenAuth = () => {
     if (userEnabled) return children
-    // if (userTrial > 0) return <TrialProvider>{children}</TrialProvider>
-    // return <SubscribeCard />
     return <TrialProvider>{children}</TrialProvider>
   }
 
